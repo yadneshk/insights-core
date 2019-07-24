5461b02edf8d99d1b60927bd71395a246dff3326
Jeremy Crafts <jcrafts@redhat.com>
2019-07-24 16:03:07 -0400

    whoops

--- insights/client/phase/v1.py
+++ insights/client/phase/v1.py
@@ -20,7 +20,7 @@ logger = logging.getLogger(__name__)
 def phase(func):
     @functools.wraps(func)
     def _f():
-        logger.info('CLIENT-RUN PID: ' + os.getpid())
+        print('CLIENT-RUN PID: ' + str(os.getpid()))
         try:
             config = InsightsConfig().load_all()
         except ValueError as e:
